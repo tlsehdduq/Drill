5a11cbbc7e2ebc19cd8d97465fbd446cc2d26374
tlsehdduq <bmbc98@naver.com>
2021-09-09 23:26:08 +0900

    Drill#04
    2018180024신동엽

---
+++ Drill04/grid.py
@@ -0,0 +1,65 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.reset()
+>>> turtle.forward(500)
+tur
+>>> tle.left(90)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    tle.left(90)
+NameError: name 'tle' is not defined
+>>> turtle.left(90)
+t
+>>> turtle.forward(500)
+turl
+>>> t
+>>> turtle.left(90)
+>>> turtle.forward(500)
+t
+>>> turtle.left(90)
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(100,0)
+>>> turtle.left(90)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.undo
+<function undo at 0x00000296786D7550>
+>>> turtle.undo()
+>>> turtle.left(90)
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(200,0)
+turlt
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(300,0)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+tu
+>>> r
+>>> turtle.penup()
+>>> turtle.goto(400,0)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(0,100)
+turtl
+>>> turtle.right(90)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(0,200)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(0,300)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> turtle.penup()
+>>> turtle.goto(0,400)
+>>> turtle.pendown()
+>>> turtle.forward(500)
+>>> 
