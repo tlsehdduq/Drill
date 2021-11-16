280f8ed430878e4633dcbe1d0d3ae29f985d1ccc
tlsehdduq <90247013+tlsehdduq@users.noreply.github.com>
2021-11-16 18:14:10 +0900

    drill12
    업데이트 영상
    https://youtu.be/wvzsyKtoHRQ

--- Drill12/bird.py
+++ Drill12/bird.py
@@ -3,13 +3,13 @@ from pico2d import *
 import game_world
 import random
 
-# 참새의 평균 비행 속도 29~40km 따라서 참새로 가정하고 속도를 30키로로 설정해주었고
+# 참새의 평균 비행 속도 29~40km 따라서 참새로 가정하고 속도를 20키로로 설정해주었고
 # 참새의 크기 20cm 20cm로 설정
-# 사람 기준 74pixel을 사용
+# 사람 기준 74pixel boy = 10 pixel 30 cm 총 2.2m
 # 전체 74pixel일때 20 cm
 
 PIXEL_PER_METER = ( 7.4/ 2.0 )  # 전체 74pixel일때 20 cm
-RUN_SPEED_KMPH = 30.0  # Km / Hour
+RUN_SPEED_KMPH = 20.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
